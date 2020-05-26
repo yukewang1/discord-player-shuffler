983abd7604d7e9d1c67daf4aa216905305e761ff
Yuke Wang <yukewang1@gmail.com>
2020-05-26 14:55:50 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -61,6 +61,23 @@ function displayQueue(ch) {
 }
 
 
+function joinQueue(person, ch, multi) {
+    if (!players.includes(person)) {
+        players.push(person);
+
+        if (multi) {
+            ch.send(`${person} has been added to the queue.`);
+        }
+        else {
+            ch.send(`${person} joined the queue.`);
+        }
+    }
+    else {
+        ch.send(`${person} is already in queue.`);
+    }
+}
+
+
 client.on('ready', () => {
     console.log('Bot connected!');
 });
@@ -77,14 +94,8 @@ client.on('message', message => {
             }
 
             case 'join': {
-                if (!players.includes(message.author)) {
-                    players.push(message.author);
-                    message.channel.send(`${message.author} joined the queue.`);
-                    displayQueue(message.channel);
-                }
-                else {
-                    message.channel.send(`${message.author} is already in queue.`);
-                }
+                joinQueue(message.author, message.channel, false);
+                displayQueue(message.channel);
                 break;
             }
 
@@ -99,6 +110,43 @@ client.on('message', message => {
                     message.channel.send(`${message.author} is not in queue.`);
                 }
                 break;
+            }
+
+            case 'kick': {
+                if (commandList.length > 2 && message.mentions.members.size > 0) {
+                    for (const n of message.mentions.members.values()) {
+                        if (players.includes(n.user)) {
+                            players.splice(players.indexOf(n.user), 1);
+                            message.channel.send(`${n.user} has been kicked from the queue.`);
+                        }
+                        else {
+                            message.channel.send(`${n.user} is not in queue.`);
+                        }
+                    }
+
+                    displayQueue(message.channel);
+                    break;
+                }
+                else {
+                    message.channel.send('Invalid *kick* command: to kick players from the quee, use *"!roll kick @player1 [@player2]"*.');
+                    break;
+                }
+            }
+
+            case 'add': {
+                if (commandList.length > 2 && message.mentions.members.size > 0) {
+                    for (const n of message.mentions.members.values()) {
+                        // console.log(n.user);
+                        joinQueue(n.user, message.channel, true);
+                    }
+
+                    displayQueue(message.channel);
+                    break;
+                }
+                else {
+                    message.channel.send('Invalid *add* command: to add players to the quee, use *"!roll add @player1 [@player2]"*.');
+                    break;
+                }
             }
 
             case 'start': {
@@ -144,7 +192,13 @@ client.on('message', message => {
             case 'help': {
                 const helpInfo = new Discord.MessageEmbed()
                     .setTitle('Available Commands')
-                    .setDescription('Use *"!roll join"* to join the queue.\nUse *"!roll exit"* to exit the queue.\nUse *"!roll start [partySize]"* to start shuffling.\nUse *"!roll displayQueue"* to display the current queue.\nUse *"!roll clearQueue"* to clear the queue.');
+                    .setDescription('Use *"!roll join"* to join the queue.\n\
+                        Use *"!roll exit"* to exit the queue.\n\
+                        Use *"!roll kick @player1 [@player2]"* to kick players from the queue.\n\
+                        Use *"!roll add @player1 [@player2]"* to add players to the queue.\n\
+                        Use *"!roll start partySize"* to start shuffling.\n\
+                        Use *"!roll displayQueue"* to display the current queue.\n\
+                        Use *"!roll clearQueue"* to clear the queue.');
                 message.channel.send(helpInfo);
 
                 break;
